d529a5ed2f98064d116d79b20a957f7377147b9f
Lisa Wagner <wagnerl@mskcc.org>
2020-11-19 09:52:29 -0500

    Refactored to work with prod user session response

--- frontend/src/redux/reducers/user/userReducer.js
+++ frontend/src/redux/reducers/user/userReducer.js
@@ -8,11 +8,11 @@ function userReducer(state = initialState, action) {
     switch (action.type) {
         case ActionTypes.FETCH_USER_SUCCESS: {
             console.log(action.user);
-
+            let role = action.user.payload.isLabMember ? 'lab_member' : 'user';
             return {
                 ...state,
-                username: action.user.username,
-                role: action.user.role,
+                username: action.user.payload.username,
+                role: role,
             };
         }
         case ActionTypes.FETCH_USER_FAIL:
