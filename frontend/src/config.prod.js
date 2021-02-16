a8ac65070dfda2d1b333fb39729767e1da8a375b
David Streid <david.streid.nyc@gmail.com>
2021-02-16 12:53:50 -0500

    Adding configs

---
+++ frontend/src/config.prod.js
@@ -0,0 +1,10 @@
+export const Config = {
+    ENV: 'production',
+    BASENAME: 'node-sample-sub',
+    NODE_API_ROOT: 'https://igodev.mskcc.org/node-sample-sub/api',
+    AUTH_URL: '/login',
+    HOME_PAGE_PATH: '/node-sample-sub',
+
+
+    VERSION: '2.1-beta',
+};
