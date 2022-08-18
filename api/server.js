7c55e58c8e6a334a2e0950014d4089e7deda72db
Stephanie DelBelso <sdelbelso@grubhub.com>
2022-08-18 08:46:25 -0400

    add more body parser options

--- api/server.js
+++ api/server.js
@@ -33,13 +33,15 @@ var publicDir = path.join(__dirname, 'public');
 
 var app = express();
 
-// app.use(cookieParser());
+app.use(cookieParser());
 const jwtInCookie = require('jwt-in-cookie');
 jwtInCookie.configure({ secret: process.env.JWT_SECRET });
 
 // middleware
 app.use(bodyparser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.raw({limit: '50mb'}));
+app.use(bodyparser.text({limit: '50mb'}));
 
 // app.use(express.json({limit: '50mb'}));
 // app.use(express.urlencoded({limit: 1024*1024*20, extended: true, parameterLimit: 50000}));
