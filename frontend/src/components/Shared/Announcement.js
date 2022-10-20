7ae8ebdb1eab89d6d4efc9ac25ed086cf19dd290
Stephanie DelBelso <sdelbelso@grubhub.com>
2022-10-20 08:58:25 -0400

    style updates

--- frontend/src/components/Shared/Announcement.js
+++ frontend/src/components/Shared/Announcement.js
@@ -4,8 +4,7 @@ const Announcement = () => {
     return (
         <div className='announcement'>
             <p>
-                Mark your calendar: IGO's Halloween Open House is Friday, October 28!
-                <a href="https://genomics.mskcc.org/halloween" target='_blank'>Learn more here</a>
+                Mark your calendar: IGO's Halloween Open House is Friday, October 28! <a href="https://genomics.mskcc.org/halloween" target='_blank'>Learn more here</a>
             </p>
         </div>
     );
