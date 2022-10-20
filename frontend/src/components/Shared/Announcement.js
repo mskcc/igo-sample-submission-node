b4a74ba32558fd5627d1cbaa48973116f9c1b754
Stephanie DelBelso <sdelbelso@grubhub.com>
2022-10-20 08:48:16 -0400

    Add halloween announcement

---
+++ frontend/src/components/Shared/Announcement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Announcement = () => {
+    return (
+        <div className='announcement'>
+            <p>
+                Mark your calendar: IGO's Halloween Open House is Friday, October 28!
+                <a href="https://genomics.mskcc.org/halloween" target='_blank'>Learn more here</a>
+            </p>
+        </div>
+    );
+};
