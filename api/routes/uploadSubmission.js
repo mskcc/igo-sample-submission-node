2a3f684fb7fdb725c9c8902b8afb1761d0c50f7a
Ojasvi1329 <ojasvipatel113@gmail.com>
2024-10-01 17:04:15 -0400

    Updated frontend for SPecies

--- api/routes/uploadSubmission.js
+++ api/routes/uploadSubmission.js
@@ -34,9 +34,21 @@ router.get('/materials',async(req,res)=>{
 }); 
 
 router.get('/species',async(req,res)=>{
+    const{materials,applications}= req.query;
     try{
-        const species = await
-        UploadSubmission.find({},{Species:1,_id:0});
+        let species = await 
+        UploadSubmission.find({},{Species:1,Material:1,Application:1,_id:0});
+        console.log("Fetched Species",species);
+        if(materials){
+            //filteredSpecies=species.filter(s=>s.materials.include(materials));
+        
+            species=species.filter(s=>s.Material&&s.Material.includes(materials));
+        }
+        if(applications){
+            //filteredSpecies=species.filter(s=>s.materials.include(materials));
+        
+            species=species.filter(s=>s.Application&&s.Application.includes(applications));
+        }
                 res.status(200).json(species);
     }catch(err){
         res.status(500).json({messaage:'Error fetching Species'+ err.message});
@@ -45,12 +57,24 @@ router.get('/species',async(req,res)=>{
 
 
 router.get('/containers',async(req,res)=>{
+    const{materials,applications,species}= req.query;
     try{
-        const containers = await
-        UploadSubmission.find({},{Containers:1,_id:0});
+        let containers = await
+        UploadSubmission.find({},{Containers:1,Species:1,Material:1,Application:1,_id:0});
+        console.log("Fetched Containers",containers);
+        if(materials){
+            //filteredSpecies=species.filter(s=>s.materials.include(materials));
+        
+            containers=containers.filter(s=>s.Material&&s.Material.includes(materials));
+        }
+        if(applications){
+            //filteredSpecies=species.filter(s=>s.materials.include(materials));
+        
+            containers=containers.filter(s=>s.Application&&s.Application.includes(applications));
+        }
                 res.status(200).json(containers);
     }catch(err){
-        res.status(500).json({messaage:'Error fetching containers'+ err.message});
+        res.status(500).json({messaage:'Error fetching Containers'+ err.message});
     }
 }); 
 
