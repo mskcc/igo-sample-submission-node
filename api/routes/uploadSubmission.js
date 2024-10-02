4ede3eedcfc43e851ca5037b0390fd30cac7b4fe
Ojasvi1329 <ojasvipatel113@gmail.com>
2024-10-02 15:19:45 -0400

    api routes to fetch unique values

--- api/routes/uploadSubmission.js
+++ api/routes/uploadSubmission.js
@@ -13,7 +13,9 @@ router.get('/submissions',async(req,res)=>{
     }
 });
 
-router.get('/applications',async(req,res)=>{
+
+
+/*router.get('/applications',async(req,res)=>{
     try{
         const applications = await
         UploadSubmission.find({},{Application:1,_id:0});
@@ -22,34 +24,79 @@ router.get('/applications',async(req,res)=>{
         res.status(500).json({messaage:'Error fetching Application'+ err.message});
     }
 }); 
+*/
+
+router.get('/applications',async(req,res)=>{
+    const {material}=req.query;
+    try{
+        let query={};
+        if(material){
+            const materials=material.split(',').map(item=>item.trim());
+           query.Material={$in:materials};
+        }
+        const applications = await
+        UploadSubmission.find(query,{Application:1,_id:0});
+        let allApplications=[];
+        applications.forEach(app=>{
+            if(app.Application){
+            allApplications=allApplications.concat(app.Application.split(",").map(item=>item.trim()));
+            }
+        });
+        const uniqueApplications=Array.from(new Set(allApplications));
+        res.status(200).json(uniqueApplications);
+    }catch(err){
+        res.status(500).json({messaage:'Error fetching Application'  + err.message});
+    }
+}); 
 
 router.get('/materials',async(req,res)=>{
+    let{application}=req.query;
+    console.log("received application:",application);
     try{
+        let query={};
+        if(application){
+            const applications=application.split(',').map(item=>item.trim());
+           query.Application={$in:applications};
+           console.log("Query used for MongoDB:",query);
+        }
         const materials = await
-        UploadSubmission.find({},{Material:1,_id:0});
-                res.status(200).json(materials);
+        UploadSubmission.find(query,{Material:1,_id:0});
+        console.log("Materials fetched:",materials);
+        let allMaterials=[];
+        materials.forEach(mat=>{
+            if(mat.Material){
+                allMaterials=allMaterials.concat(mat.Material.split(",").map(item=>item.trim()));
+            }
+        });
+        const uniqueMaterials=Array.from(new Set(allMaterials));
+        console.log("Unique Materials fetched:",uniqueMaterials);
+        res.status(200).json(uniqueMaterials);
     }catch(err){
         res.status(500).json({messaage:'Error fetching Materials'+ err.message});
     }
 }); 
 
 router.get('/species',async(req,res)=>{
-    const{materials,applications}= req.query;
     try{
-        let species = await 
-        UploadSubmission.find({},{Species:1,Material:1,Application:1,_id:0});
+        let query={};
+        if(req.query.material){
+            const materials=req.query.material.split(',').map(item=>item.trim());
+           query.Material={$in:materials};
+        }
+        if(req.query.application){
+            const applications=req.query.application.split(',').map(item=>item.trim());
+            query.Application={$in:applications};
+        }
+        const species = await 
+        UploadSubmission.find(query,{Species:1,_id:0});
         console.log("Fetched Species",species);
-        if(materials){
-            //filteredSpecies=species.filter(s=>s.materials.include(materials));
         
-            species=species.filter(s=>s.Material&&s.Material.includes(materials));
-        }
-        if(applications){
-            //filteredSpecies=species.filter(s=>s.materials.include(materials));
-        
-            species=species.filter(s=>s.Application&&s.Application.includes(applications));
-        }
-                res.status(200).json(species);
+            const allspecies=species.reduce((acc,item)=>{
+                const SpeciesArray=item.Species.split(",").map(item=>item.trim());
+                return acc.concat(SpeciesArray);
+            },[]);
+            const uniqueSpecies=Array.from(new Set(allspecies));
+                res.status(200).json(uniqueSpecies);
     }catch(err){
         res.status(500).json({messaage:'Error fetching Species'+ err.message});
     }
